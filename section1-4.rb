6ab32c27a13f26d21e2413cf3e5138efc24b5ca8
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-30 22:44:57 +0900

    [start]クラスの拡張？

---
+++ section1-4.rb
@@ -0,0 +1,25 @@
+# コンテンツ概要
+# コード例で生成している各オブジェクトごとにlifeメソッドを定義しましょう。
+# lifeメソッドのおこなう処理は、コメントアウトされている出力例から推測してください。
+
+# この演習を回答するのに必要と思われる時間は、10分です。
+
+# 演習
+# 以下のコードがコメント部分の通りの出力となるように、（A）の部分に当てはまる適切なコードを書いてください。
+
+class Planet
+  def life?
+    puts "Who knows?"
+  end
+end
+ 
+pluto = Planet.new()
+earth = Planet.new()
+mars = Planet.new()
+ 
+# (A)
+
+ 
+pluto.life?()  #=> Probably not.
+earth.life?()  #=> Positively!
+mars.life?()   #=> Who knows?
